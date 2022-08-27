c310e38dbb630bb3236a804f52c441b1df5356ea
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-08-27 16:09:05 -0600

    tested function for displaying 7 in text box

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //alert("hello")
+
+//testing button function
+
+const sevenButton = document.getElementById("seven-btn");
+const numberBox = document.getElementById("number-box");
+
+console.log(sevenButton);
+console.log(numberBox);
+
+sevenButton.addEventListener('click', pressSeven);
+
+function pressSeven() {
+    numberBox.value = 7;
+}
